a909f4ba77cb3da774e4e1448978c2c1ee3fa808
Isaac Goldberg <igoldbergvt@gmail.com>
2013-06-20 16:52:33 -0400

    added ability to track number of uses with Google Analytics

--- agent.js
+++ agent.js
@@ -255,4 +255,12 @@ if (typeof jQuery !== 'undefined' && typeof R !== 'undefined' && $(".section.rdi
             updateUI();
         }
     });
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-41381466-2', 'rdio.com');
+    ga('send', 'pageview');
 }
